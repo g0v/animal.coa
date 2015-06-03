e9f90694085a785d9d265850fb9cb513c16ab03b
Tsai Wenja <agon978@gmail.com>
2015-06-03 17:38:15 +0800

    Add Tainan city.

---
+++ 台南市-小嘉/lost_pets/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for lost_pets project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'lost_pets'
+
+SPIDER_MODULES = ['lost_pets.spiders']
+NEWSPIDER_MODULE = 'lost_pets.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'lost_pets (+http://www.yourdomain.com)'
