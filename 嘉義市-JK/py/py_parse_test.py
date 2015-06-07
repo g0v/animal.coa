0d5ce31464109c56a8ad01801c7ee915e9fde238
superjk <jimmi.fly@gmail.com>
2015-06-07 22:18:56 +0800

    fix bug with chip number

--- 嘉義市-JK/py/py_parse_test.py
+++ 嘉義市-JK/py/py_parse_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from html.parser import HTMLParser
 from urllib.request import urlopen  
-from urllib import parse
+#from urllib import parse
 import csv
 import time
 
@@ -9,29 +9,30 @@ class MyHTMLParser(HTMLParser):
     dict_tag = {'捕捉日期': 0, '捕捉地點': 1, '品種': 2, '性別': 3, '體型': 4, '捕捉來源': 5, '備註': 6, '晶片號碼': 7, '照片': 8}    
     each_animal_data=['無資料','無資料','無資料','無資料','無資料','無資料','無資料','無資料','無資料']   
     inner_dataset=[each_animal_data,each_animal_data,each_animal_data,each_animal_data,each_animal_data]
-    row_data=""
     start_key = 0
     i=0
     def handle_starttag(self, tag, attrs):
         if tag == "img" and attrs[0][1].split(r'/')[1]=='upload' and len(attrs[0][1].split(r'banner')) < 2:
             self.each_animal_data[self.dict_tag['照片']] = 'http://www.dog.dias.com.tw' + attrs[0][1]
-    def parse_data(self, data):
-        if len(data)==10 and data[4]=="-":
-            self.each_animal_data[self.dict_tag['捕捉日期']] = data
-        elif data[:5]=="捕捉地點：":
-            self.each_animal_data[self.dict_tag['捕捉地點']] = data[5:len(data)]
-        elif data[:3]=="品種：": 
-            self.each_animal_data[self.dict_tag['品種']] = data[3:len(data)]
-        elif data[:3]=="體型：": 
-            self.each_animal_data[self.dict_tag['體型']] = data[3:len(data)]
-        elif data[:3]=="性別：": 
-            self.each_animal_data[self.dict_tag['性別']] = data[3:len(data)]
-        elif data[:5]=="捕捉來源：": 
-            self.each_animal_data[self.dict_tag['捕捉來源']] = data[5:len(data)]
-        elif data[:3]=="備註：": 
-            if "(晶片:" in data:
-                self.each_animal_data[self.dict_tag['晶片號碼']] = data.split(":")[1][0:-1]
-            self.each_animal_data[self.dict_tag['備註']] = data[3:len(data)]
+    def parse_data(self, datax):
+        if len(datax)==10 and datax[4]=="-":
+            self.each_animal_data[self.dict_tag['捕捉日期']] = datax
+        elif datax[:5]=="捕捉地點：":
+            self.each_animal_data[self.dict_tag['捕捉地點']] = datax[5:len(datax)]
+        elif datax[:3]=="品種：": 
+            self.each_animal_data[self.dict_tag['品種']] = datax[3:len(datax)]
+        elif datax[:3]=="體型：": 
+            self.each_animal_data[self.dict_tag['體型']] = datax[3:len(datax)]
+        elif datax[:3]=="性別：": 
+            self.each_animal_data[self.dict_tag['性別']] = datax[3:len(datax)]
+        elif datax[:5]=="捕捉來源：": 
+            self.each_animal_data[self.dict_tag['捕捉來源']] = datax[5:len(datax)]
+        elif datax[:3]=="備註：": 
+            self.each_animal_data[self.dict_tag['備註']] = datax[3:len(datax)]
+            if "(晶片:" in datax:
+                self.each_animal_data[self.dict_tag['晶片號碼']] = datax.split(":")[1][0:-1]
+            else:
+                self.each_animal_data[self.dict_tag['晶片號碼']] = "無資料"
     def handle_data(self, data):
         if self.start_key == 1:
             self.parse_data(data)
@@ -44,7 +45,7 @@ class MyHTMLParser(HTMLParser):
             htmlString = htmlBytes.decode("utf-8")
             self.feed(htmlString)
             data_set.extend(self.inner_dataset)
-    def handle_comment(self,data):
+    def handle_comment(self, data):
         if data==" content start ":
             self.start_key= 0
             self.i=0
@@ -69,11 +70,11 @@ k = 1
 while k <= 16:
     aniset =[]
     parser = MyHTMLParser()
-    parser.getLinks("http://www.dog.dias.com.tw/index.php?op=announcement&page="+str(k),aniset)
+    parser.getLinks("http://www.dog.dias.com.tw/index.php?op=announcement&page="+str(k),aniset)    
     print(aniset)
     f = open("eggs.csv",'a',encoding='utf-8')  
     w = csv.writer(f, delimiter=',')      
-    w.writerows(aniset)   
+    w.writerows(aniset)
     f.close()
     time.sleep(3)
     k = k+1
